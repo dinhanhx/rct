02571fcfa05a8292a02cd2de25fa368f985634a9
Dinh Anh <dinhanhx@gmail.com>
2023-03-02 09:51:45 +0700

    Format

--- rct/clean.py
+++ rct/clean.py
@@ -8,8 +8,10 @@ from tqdm import tqdm
 COSPLAY_JSONL = Path('data/cosplay.jsonl')
 CLEAN_COSPLAY_JSONL = Path('data/clean_cosplay.jsonl')
 
-with (jsonl.open(COSPLAY_JSONL, 'r') as cosplay_fp,
-      jsonl.open(CLEAN_COSPLAY_JSONL, 'a') as clean_cosplay_fp):
+with (
+    jsonl.open(COSPLAY_JSONL, 'r') as cosplay_fp,
+    jsonl.open(CLEAN_COSPLAY_JSONL, 'a') as clean_cosplay_fp,
+):
     for d in tqdm(cosplay_fp):
         # Regex \[(.*?)\] or \[.*\] to detect text enclosed by square brackets
         # such as [self]
